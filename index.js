dfbaa38a375f635254217b017a4f31decd28714a
Jesse Gibson <overlord@Marvin.local>
2015-10-14 00:29:22 -0600

    Added a method to grab every line on either an x or y axis. Used to find dangers to players and kill them on contact
    (which is proving more difficult). Also added a distinction between speed and direction. Oh, and super powers.
    Current problems include kill order (who should die first on a collision, determining whether the next should or not), moving things over to percent based systems (for varying screen types trying to collaborate on the same data) and plugging in gun to drive the system. Collision detection is what I forsee as the most complex problem.

--- index.js
+++ index.js
@@ -5,58 +5,90 @@
   'use strict';
 
   function Player(color, x, y) {
-    this.x = x;
-    this.y = y;
-    this.color = color;
-    this.width = 1;
-    this.velocity = 3;
-    this.direction = 'x';
-    this.history = [{
+    var axis = 'x',
+      player = this;
+    player.x = x;
+    player.y = y;
+    player.color = color;
+    player.width = 0;
+    player.speed = 1;
+    player.direction = 1;
+    player.axis = axis;
+    player.killed = false;
+    player.power = {
+      memColor: player.color,
+      memSpeed: player.speed,
+      boost: null
+    };
+    player.history = [{
       x: x,
-      y: y
+      y: y,
+      axis: axis
     }];
   }
 
   Player.prototype = {
     constructor: Player,
-    move: function () {
-      this[this.direction] += this.velocity;
+    toString: function () {
+      return "Player " + this.power.memColor;
     },
-    turn: function (direction) {
-      var player = this,
-        velocity = Math.abs(player.velocity),
-        setDirection = function (axis, velocity) {
-          if (player.direction === axis) {
-            return;
-          }
-          player.direction = axis;
-          player.velocity = velocity;
-
-          player.history.push({
-            x: player.x,
-            y: player.y
-          });
-        };
-
-      switch (direction.toLowerCase()) {
-      case 'up':
-        setDirection('y', -velocity);
-        break;
-      case 'down':
-        setDirection('y', velocity);
-        break;
-      case 'left':
-        setDirection('x', -velocity);
-        break;
-      case 'right':
-        setDirection('x', velocity);
-        break;
-      default:
-        return;
+    move: function () {
+      var step = this.speed * this.direction;
+      this[this.axis] += step;
+      return this;
+    },
+    turn: function (axis, direction) {
+      var player = this;
+      if (player.axis === axis) {
+        return this;
       }
+      player.history.push({
+        axis: player.axis,
+        x: player.x,
+        y: player.y
+      });
+
+      player.direction = direction;
+      player.axis = axis;
+      return this;
     },
     kill: function () {
-      this.velocity = 0;
+      var player = this;
+      if (player.killed) {
+        return player;
+      }
+      player.killed = new Date();
+      console.log((player + " is dead").toUpperCase());
+      player.unBoost();
+      player.speed = 0;
+      return this;
+    },
+    boost: function () {
+      var player = this;
+      if (player.power.boost || player.killed) {
+        return this;
+      }
+      player.power.boost = setInterval(function () {
+        player.color = '#' + Math.random().toString(16).substr(-6);
+      }, 100);
+
+      player.speed = player.speed * 2.5;
+      setTimeout(function () {
+        if (player.killed) {
+          return;
+        }
+        player.unBoost();
+      }, 3000);
+
+      return this;
+    },
+    unBoost: function () {
+      var player = this;
+      clearInterval(player.power.boost);
+      player.color = player.power.memColor;
+      player.speed = player.power.memSpeed;
+      player.power.boost = null;
+      return this;
     }
   };
 
@@ -67,28 +99,31 @@
     ctx = canvas.getContext('2d'),
     model,
     view,
-    controller;
+    controller,
+    stopped = false;
 
   view = {
     color: {
       background: '#101418',
-      player: ['green', 'lightblue', 'red', 'yellow']
+      player: ['green', 'blue', 'red', 'yellow']
     },
     render: function () {
-      view.clear();
-      view.drawWalls();
+      view.clear().drawWalls();
       model.players.map(view.drawPlayer);
+      return this;
     },
     clear: function () {
       ctx.beginPath();
       ctx.rect(0, 0, canvas.width, canvas.height);
       ctx.fillStyle = view.color.background;
       ctx.fill();
+      return this;
     },
     resizeCanvas: function () {
       canvas.width = window.innerWidth;
       canvas.height = window.innerHeight;
       view.render();
+      return this;
     },
     drawPlayer: function (player) {
       var offset = player.width / 2;
@@ -96,12 +131,13 @@
       ctx.rect(player.x - offset, player.y - offset, player.width, player.width);
       ctx.fillStyle = player.color;
       ctx.fill();
+      return this;
     },
     drawWalls: function () {
       model.players.map(function (player, i) {
         ctx.beginPath();
         ctx.strokeWidth = 1;
-        ctx.strokeStyle = view.color.player[i];
+        ctx.strokeStyle = player.color;
         ctx.moveTo(player.history[0].x, player.history[0].y);
         player.history.map(function (coord) {
           ctx.lineTo(coord.x, coord.y);
@@ -110,72 +146,148 @@
         ctx.stroke();
         ctx.closePath();
       });
+      return this;
     }
   };
 
   model = {
     players: [],
     movePlayers: function () {
-      model.players.map(function (player) {
+      if (stopped) {
+        return;
+      }
+      model.testForCollisions();
+      model.players.forEach(function (player) {
         player.move();
-        if (model.hasCollided(player)) {
+      });
+
+      view.render();
+      requestAnimationFrame(model.movePlayers);
+      return this;
+    },
+    findLines: function (axis) {
+      var lines = [];
+
+      function addLines(start, end, player) {
+        if (end.axis !== axis) {
+          return;
+        }
+        var opposite = (axis === 'x') ? 'y' : 'x',
+          intermediate;
+        if (start > end) {
+          intermediate = start;
+          start = end;
+          end = intermediate;
+        }
+        lines.push({
+          start: start[axis],
+          end: end[axis],
+          offset: end[opposite],
+          owner: player
+        });
+      }
+
+      model.players.forEach(function (player) {
+        var start, end, i, lastEntry = player.history.length - 1,
+          lastTurn = player.history[lastEntry];
+
+        for (i = 0; i < lastEntry; i++) {
+          start = player.history[i];
+          end = player.history[i + 1];
+
+          addLines(start, end, player);
+        }
+        // Include the current trajectory
+        addLines(lastTurn, {
+          x: player.x,
+          y: player.y,
+          axis: player.axis
+        }, player);
+
+      });
+      return lines;
+    },
+    testForCollisions: function () {
+      var lines = {
+        x: model.findLines('x'),
+        y: model.findLines('y')
+      };
+
+      model.players.forEach(function (player) {
+        if (player.killed) {
+          return this;
+        }
+        //      if (player.y === 199) {
+        //        debugger;
+        //      }
+        var perpendicular = player.axis === 'x' ? 'y' : 'x',
+          lastEntry = player.history.length - 1,
+          collision = lines[perpendicular].filter(function (line) {
+            if (line.owner.killed) {
+              return false;
+            }
+            var isAfterBeginning = line.start < player[perpendicular],
+              isBeforeEnd = line.end > player[perpendicular],
+              isInWay = isAfterBeginning && isBeforeEnd,
+
+              trajectoryBeginsBeforeLine = player.history[lastEntry][player.axis] < line.offset,
+              trajectoryEndsAfterLine = player[player.axis] > line.offset,
+              isOverlapping = trajectoryBeginsBeforeLine && trajectoryEndsAfterLine;
+
+            return isInWay && isOverlapping;
+          });
+
+        if (collision.length !== 0) {
           player.kill();
         }
       });
-      view.render();
-      requestAnimationFrame(model.movePlayers);
-    },
-    hasCollided: function (player) {
-      var lastPosition = player.history[player.history.length - 1],
-        direction = player.direction,
-        allWalls = [],
-        collision = false;
-
-      model.players.forEach(function (player) {
-        var walls = [];
-        player.history.sort(function (last, current) {
-          var wall = false;
-          if (last[direction] === current[direction]) {
-            wall = [last[direction], current[direction]];
-          }
-          if (wall) {
-            walls.push(wall);
-          }
-        });
-        allWalls = allWalls.concat(walls);
-      });
-
-      allWalls.forEach(function (wall) {
-        // TODO: Check to see if there is overlap
-      });
-
-      return false;
+      return this;
     }
   };
+
 
   controller = {
     init: function () {
       view.resizeCanvas();
       controller.setPlayers();
+      model.movePlayers();
       window.addEventListener('resize', view.resizeCanvas);
       document.body.addEventListener('keydown', controller.gameInput);
-      model.movePlayers();
+      return this;
     },
     setPlayers: function () {
       model.players = view.color.player.map(function (color, i) {
         return new Player(color, 100, ++i * 100);
       });
+      return this;
     },
     gameInput: function (e) {
       var player = model.players[0],
-        direction;
+        arrow,
+        axis;
       if (e.keyCode === 0) {
-        return;
+        return this;
+      }
+      if (e.keyCode === 32) {
+        player.boost();
       }
       if (e.key.match(/Arrow/)) {
-        direction = e.key.match(/^Arrow(\w*)$/)[1];
-        player.turn(direction);
+        arrow = e.key.match(/^Arrow(\w*)$/)[1];
+
+        switch (arrow.toLowerCase()) {
+        case 'up':
+          return player.turn('y', -1);
+        case 'down':
+          return player.turn('y', 1);
+        case 'left':
+          return player.turn('x', -1);
+        case 'right':
+          return player.turn('x', 1);
+        default:
+          return this;
+        }
       }
+      return this;
     }
   };
 
