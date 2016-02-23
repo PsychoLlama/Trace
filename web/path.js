54d3e7b94f63c1aff4cea9983971c0a86a78915f
Jesse Gibson <Jesse_Gibson@me.com>
2016-02-22 17:24:37 -0700

    Remove redundant code

--- web/path.js
+++ web/path.js
@@ -4,28 +4,6 @@ var find = require('./find');
 var sort = require('./sort');
 var line = require('./line');
 
-function line(start, end) {
-	if (!start || !end) {
-		return null;
-	}
-	var temp, perp, axis = start.axis;
-	perp = (axis === 'x') ? 'y' : 'x';
-
-	// always record going from top to bottom, left to right
-	if (start[axis] > end[axis]) {
-		temp = start;
-		start = end;
-		end = temp;
-	}
-
-	return {
-		start: start[axis],
-		end: end[axis],
-		offset: end[perp],
-		axis: axis
-	};
-}
-
 module.exports = function path(player) {
 	var	position, lines;
 	if (!player) {
